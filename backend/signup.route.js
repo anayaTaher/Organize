a30ede6992ceb1f0bb64a4c2b138221f58fd6c20
anaya <anaya.taher@gmail.com>
2021-12-27 22:49:50 +0200

    Taher And Badawi5

--- backend/signup.route.js
+++ backend/signup.route.js
@@ -58,7 +58,7 @@ router.post('/forgotPassword', async (request, response) => {
 		const secret = JWT_Secret + user.password
 		const payload = {email: user.email, id: user._id}
 		const token = jwt.sign(payload, secret, {expiresIn: "15m"})
-		const link = `http://192.168.1.242:3000/reset-password/${user._id}/${token}`
+		const link = `http://localhost:3000/reset-password/${user._id}/${token}`
 		confirmLinkModel.createNewLink(user._id, token)
 		
 		let transporter = nodemailer.createTransport({
