eb868da0b22f96eb15e28ec9301a5842a408f2a7
anaya <anaya.taher@gmail.com>
2021-12-27 23:54:59 +0200

    Taher And Badawi6

--- frontend/src/components/MessageForm.jsx
+++ frontend/src/components/MessageForm.jsx
@@ -4,13 +4,15 @@ import SendIcon from '@mui/icons-material/Send'
 
 const MessageForm = ({handleSubmit, text, setText, setImg}) => <>
 	<form className="message_form" onSubmit={handleSubmit}>
-		<label htmlFor="img"> <AttachmentIcon style={{fontSize: "40px", color: "#26a69a"}}/></label>
+		<label htmlFor="img"> <AttachmentIcon sx={{
+			fontSize: "40px", color: "#26a69a", ":hover": {cursor: "pointer"}
+		}}/></label>
 		<input onChange={e => setImg(e.target.files[0])} type="file" id="img" style={{display: "none"}}/>
 		<input type="text" placeholder="Aa" value={text} onChange={e => setText(e.target.value)} style={{
 			borderRadius: "10px",
 			width: "100%"
 		}}/>
-		<SendIcon style={{fontSize: "40px", color: "#26a69a"}}/>
+		<SendIcon onClick={handleSubmit} sx={{fontSize: "40px", color: "#26a69a", ":hover": {cursor: "pointer"}}}/>
 	</form>
 </>
 
